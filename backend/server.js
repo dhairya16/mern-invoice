ccd99a6ffc76983c7947c733f86c2230700f5943
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2024-02-25 20:33:53 +0530

    Logging setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import chalk from "chalk";
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import { morganMiddleware, systemLogs } from "./utils/Logger.js";
 
 const app = express();
 
@@ -12,6 +13,8 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
+
+app.use(morganMiddleware);
 
 app.get("/api/v1/test", (req, res) => {
   res.json({ Hi: "Welcome to the Invoice App" });
@@ -24,5 +27,8 @@ app.listen(PORT, () => {
     `${chalk.green.bold("✅")} 👍 Server running in ${chalk.yellow.bold(
       process.env.NODE_ENV
     )} mode on port ${chalk.blue.bold(PORT)}`
+  );
+  systemLogs.info(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
   );
 });
