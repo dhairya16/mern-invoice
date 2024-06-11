a62deed51e43650d618bb21545b95c3b12975048
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2024-06-11 23:39:52 +0530

    Resend verification token controller added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,6 +4,7 @@ import registerUser from "../controllers/auth/registerController.js";
 import verifyUserEmail from "../controllers/auth/verifyEmailController.js";
 import loginUser from "../controllers/auth/loginController.js";
 import newAccessToken from "../controllers/auth/refreshTokenController.js";
+import resendEmailVerificationToken from "../controllers/auth/resendVerifyEmailController.js";
 
 import { loginLimiter } from "../middleware/apiLimiter.js";
 
@@ -13,5 +14,6 @@ router.post("/register", registerUser);
 router.get("/verify/:emailToken/:userId", verifyUserEmail);
 router.post("/login", loginLimiter, loginUser);
 router.get("/new_access_token", newAccessToken);
+router.post("/resend_email_token", resendEmailVerificationToken);
 
 export default router;
