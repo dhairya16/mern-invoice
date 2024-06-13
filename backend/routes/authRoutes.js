3983c34fb636fad570507af372ca0ed2fd0364e1
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2024-06-13 19:46:48 +0530

    Logout controller

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -9,6 +9,7 @@ import {
   resetPasswordRequest,
   resetPassword,
 } from "../controllers/auth/passwordResetController.js";
+import logoutUser from "../controllers/auth/logoutController.js";
 
 import { loginLimiter } from "../middleware/apiLimiter.js";
 
@@ -21,5 +22,6 @@ router.get("/new_access_token", newAccessToken);
 router.post("/resend_email_token", resendEmailVerificationToken);
 router.post("/reset_password_request", resetPasswordRequest);
 router.post("/reset_password", resetPassword);
+router.get("/logout", logoutUser);
 
 export default router;
