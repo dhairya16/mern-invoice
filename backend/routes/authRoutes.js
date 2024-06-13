73b464bb7076d76e5288a53f3831141ef5d19468
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2024-06-13 19:29:52 +0530

    Password reset functionality added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,6 +5,10 @@ import verifyUserEmail from "../controllers/auth/verifyEmailController.js";
 import loginUser from "../controllers/auth/loginController.js";
 import newAccessToken from "../controllers/auth/refreshTokenController.js";
 import resendEmailVerificationToken from "../controllers/auth/resendVerifyEmailController.js";
+import {
+  resetPasswordRequest,
+  resetPassword,
+} from "../controllers/auth/passwordResetController.js";
 
 import { loginLimiter } from "../middleware/apiLimiter.js";
 
@@ -15,5 +19,7 @@ router.get("/verify/:emailToken/:userId", verifyUserEmail);
 router.post("/login", loginLimiter, loginUser);
 router.get("/new_access_token", newAccessToken);
 router.post("/resend_email_token", resendEmailVerificationToken);
+router.post("/reset_password_request", resetPasswordRequest);
+router.post("/reset_password", resetPassword);
 
 export default router;
