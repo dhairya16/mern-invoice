a189dd7ae721a8db68292a81a4b25cd7e9e52007
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2024-06-17 15:38:28 +0530

    Get all user accounts for admin added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
+
 import checkAuth from "../middleware/checkAuthMiddleware.js";
+import role from "../middleware/roleMiddleware.js";
+
 import getUserProfile from "../controllers/user/getUserProfile.js";
 import updateUserProfile from "../controllers/user/updateUserProfile.js";
 import deleteMyAccount from "../controllers/user/deleteMyAccount.js";
+import getAllUserAccounts from "../controllers/user/getAllUserAccounts.js";
 
 const router = express.Router();
 
@@ -11,5 +15,9 @@ router
   .get(checkAuth, getUserProfile)
   .patch(checkAuth, updateUserProfile)
   .delete(checkAuth, deleteMyAccount);
+
+router
+  .route("/all")
+  .get(checkAuth, role.checkRole(role.ROLES.Admin), getAllUserAccounts);
 
 export default router;
